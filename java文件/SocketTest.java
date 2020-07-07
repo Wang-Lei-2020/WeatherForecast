5bb46be9443950ebeddd3b6f433fe9b4fe8d2550
Wang Lei <1327578214@qq.com>
2020-07-08 00:34:09 +0800

    python与java网络连接以及json数据传输和数据处理

--- java文件/SocketTest.java
+++ java文件/SocketTest.java
@@ -9,12 +9,41 @@ import java.net.UnknownHostException;
 public class SocketTest {
 
 	public static void main(String[] args) {
-	 	String i = receive(7,15);
+		//获取Json数据
+	 	String i = receive(7,16);
 	 	System.out.print(i);
+	 	//分割字符串
+	 	String[] data = i.split(",");
+	 	System.out.println();
+	 	//得到String类型的数据
+	 	for(int n = 0;n < 14;n++) {
+	 		if(n == 0) {
+	 			data[n] = cut(data[n],15);
+	 		}
+	 		else if(n == 13) {
+	 			data[n] = cut(data[n],14);
+	 			data[n] = data[n].substring(0, data[n].length()-2);
+	 		}
+	 		else {
+	 	        data[n] = cut(data[n],14);
+	 		}
+	 	  System.out.println(data[n]);
+	 	}
+	 	
+	 	//得到double类型的气温数据(摄氏度)
+	 	double[] t = new double[14];
+	 	for(int n = 0;n < 14;n++) {
+	 		t[n] = Double.parseDouble(data[n]);
+	 		//华氏度变为摄氏度
+	 		t[n] = (t[n]-32)*5/9;
+	 		System.out.println(t[n]);
+	 	}
+	 	
 	}
 
 	public static String receive(int month,int day) {
-		String ip = "127.0.0.1";        // 设置发送地址和端口号
+		// 设置发送地址和端口号
+		String ip = "127.0.0.1";
 		int port = 9999;
 		Socket socket = null;
 		try {
@@ -24,24 +53,37 @@ public class SocketTest {
 			InputStream in = socket.getInputStream();   //读取数据
 			// 获取输出流
 			OutputStream out = socket.getOutputStream(); // 发送数据
-			// 包装输入流，输出流，包装一下可以直接传输字符串，不包装的话直接使用InputStream和OutputStream只能直接传输byte[]类型数据
 			BufferedReader inRead = new BufferedReader(new InputStreamReader(in));
-//			PrintWriter outWriter = new PrintWriter(out);
 			
 			// 发送数据
 			out.write(month);
 			out.write(day);
 		
 			// 接受应答
-			String result = inRead.readLine();  // 使用了包装后的输入流方便读取消息
+			String result = inRead.readLine();  
+		    
 			return result;
+			
 		} catch(UnknownHostException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		return "defeat";
+		return "default";
 	}
 	
+	//去掉字符串的前几位
+	public static String cut(String origin, int count) {    
+		if (origin == null || origin.length() < count) {        
+			return null;    
+			}    
+		char[] arr = origin.toCharArray();    
+		char[] ret = new char[arr.length - count];    
+		for (int i = 0; i < ret.length; i++) {        
+			ret[i] = arr[i + count];    
+			}    
+		
+		return String.copyValueOf(ret);
+		}
+	
 }
-
--- socketTest.py
+++ socketTest.py
@@ -1,10 +1,10 @@
 import socket
 import pandas as pd
-# 在构建socket的时候需要用到ip和端口，必须是元组的形式。
-# 另外，因为是本机上的两个程序通信，所以设置成localhost，
-# 如果要和网络上的其他主机进行通信，则填上相应主机的ip地
-# 址，端口的话随便设置一个，不要和已知的一些服务冲突就行
-address = ('127.0.0.1', 9999)
+import statsmodels.api as sm
+from mod_timeseries.process_json import format_json
+import json
+
+address = ('127.0.0.1', 9999)  #ip地址和端口
 # 创建socket对象，同时设置通信模式，AF_INET代表IPv4，SOCK_STREAM代表流式socket，使用的是tcp协议
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 绑定到我们刚刚设置的ip和端口元组，代表我们的服务运行在本机的9999端口上
@@ -17,15 +17,65 @@ while True:
 	client, addr = server.accept()
 	month = client.recv(13)  # 代表从发过来的数据中读取13byte的数据
 	day = client.recv(4)   # 再接收4byte数据
-	month = int().from_bytes(month, byteorder='big', signed=True)
+	month = int().from_bytes(month, byteorder='big', signed=True) #接受的数据类型为byte 转换为int
 	day = int().from_bytes(day, byteorder='big', signed=True)
 	print(month)
 	print(day)
+
+	#从文件中查询该日期以后七天的气温数据
 	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
 	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
-	print(data)
-	#for i in range()
+	tmax1 = data['tmax'].values[0]
+	tmin1 = data['tmin'].values[0]
 
-	client.sendall(b'3')    # 发送消息给客户端，发送的消息必须是byte类型
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax2 = data['tmax'].values[0]
+	tmin2 = data['tmin'].values[0]
+
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax3 = data['tmax'].values[0]
+	tmin3 = data['tmin'].values[0]
+
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax4 = data['tmax'].values[0]
+	tmin4 = data['tmin'].values[0]
+
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax5 = data['tmax'].values[0]
+	tmin5 = data['tmin'].values[0]
+
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax6 = data['tmax'].values[0]
+	tmin6 = data['tmin'].values[0]
+
+	day = day + 1
+	data = pd.read_csv('F:/Git/Weather/beijing.csv', parse_dates=['date'])
+	data.query("date.dt.day == @day & date.dt.month == @month", inplace=True)
+	tmax7 = data['tmax'].values[0]
+	tmin7 = data['tmin'].values[0]
+
+	temp = [tmax1,tmin1,tmax2,tmin2,tmax3,tmin3,tmax4,tmin4,tmax5,tmin5,tmax6,tmin6,tmax7,tmin7]
+	temp = pd.Series(temp)
+	temp.index = pd.Index(sm.tsa.datetools.dates_from_range(str(1), str(14)))
+
+	print(temp)
+	temp.to_json('weather.json',date_format = 'iso')
+	temp_json = format_json('weather.json', str(month), str(day-6))
+
+	print(temp_json)
+
+	client.send(bytes(repr(json.dumps(temp_json)).encode('utf-8')))
+
+	#client.sendall(b'3')    # 发送消息给客户端，发送的消息必须是byte类型
 	client.close()        # 关闭连接
 server.close()
